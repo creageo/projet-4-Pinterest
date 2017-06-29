69eb62abd95cec02641753c3b26de9566ccd4d73
creageo <geoffreymarique@gmail.com>
2017-06-29 12:56:52 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,62 +1,82 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
 
+	<!-- lien vers bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- lien vers mon css -->
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- charactrere spécial -->
+	<meta charset="utf-8">
+	<!-- titre dans l'onglet -->
 	<title>Pinterest</title>
 
 </head>
+
 <body>
 
-<hr>
+	<!-- ligne horizontale -->
+	<hr>
+	<!-- titre principale -->
+	<h1>Pinterest</h1>
 
-<h1>Pinterest</h1>
+	<hr>
 
-<hr>
+	<?php
 
-<?php
+	if(isset($_FILES['avatar'])){
 
-if(isset($_FILES['avatar'])){
+	$dossier = 'upload/';
+	$fichier = $dossier . basename($_FILES['avatar']['name']);
+	$extensions = array('jpg','jpeg','png','gif','WebP');
+	$imagePath_parts = pathinfo($_FILES['avatar']['name']);
+	$extentionImage = $imagePath_parts['extension'];
 
-$dossier = 'upload/';
-$fichier = $dossier . basename($_FILES['avatar']['name']);
-$extensions = array('jpg','jpeg','png','gif','WebP');
-$imagePath_parts = pathinfo($_FILES['avatar']['name']);
-$extentionImage = $imagePath_parts['extension'];
-
-if (!in_array($extentionImage, $extensions)) {
-	$erreur = 'ONLY FILETYPE : .jpg .jpeg .png .gif .WebP';
-}
-
-	if (move_uploaded_file($_FILES['avatar']['tmp_name'], $fichier)) {
-		echo 'Envoie du fichier : ok';
-		echo "<br>";
+	if (!in_array($extentionImage, $extensions)) {
+		$erreur = 'ONLY FILETYPE : .jpg .jpeg .png .gif .WebP';
 	}
 
-	else{
-		echo 'Envoie du fichier : echec';
-		echo "<br>";
+		if (move_uploaded_file($_FILES['avatar']['tmp_name'], $fichier)) {
+			echo 'Envoie du fichier : ok';
+			echo "<br>";
+		}
+
+		else{
+			echo 'Envoie du fichier : echec';
+			echo "<br>";
+		}
+
 	}
 
-}
+	$dossier = 'upload/';
+	$contenu_du_dossier = scandir( $dossier );
 
-$dossier = 'upload/';
-$contenu_du_dossier = scandir( $dossier );
+	foreach( $contenu_du_dossier as $value ){
+	if ( 0 != strpos($value, '.') ){
+	echo '<img src="'.$dossier. '/'. $value.'" alt="" width="200">';
+	}
+	}
 
-foreach( $contenu_du_dossier as $value ){
-if ( 0 != strpos($value, '.') ){
-echo '<img src="'.$dossier. '/'. $value.'" alt="" width="200">';
-}
-}
+	?>
 
-?>
+	<hr>
+	<!-- début de mon formulaire d'envoie multimedia et lien vers mon fichier php, avec la methode post -->
+	<form method="POST" enctype="multipart/form-data" class="form-inline">
 
-<hr>
+		<div class="form-group">
+		<!-- image a envoyer -->
+		<input type="file" id="exampleInputFile" name="avatar">
+		</div>
 
-<form method="POST" enctype="multipart/form-data">
-	<input type="file" name="avatar">
-	<input type="submit" name="bouton" value="Envoyer le fichier">
-</form>
+		<div class="form-group">
+		<!-- bouton d'envoie du formulaire -->
+		<input class="btn btn-default" type="submit" name="bouton">
+		</div>
+
+	</form>
 
 </body>
+
 </html>
